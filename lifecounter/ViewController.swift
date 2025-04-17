851a7d960b841ddf45475b17126420c599f6ace5
Kira Brodsky <kbrods@uw.edu>
2025-04-16 19:55:35 -0700

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Kira Brodsky on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
